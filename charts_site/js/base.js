3ffa576697b6464ed021a37cc88c5f2cd0f1cbe8
Eugene Suprun <suprune20@gmail.com>
2020-04-16 21:00:19 +0300

    Карта

--- charts_site/js/base.js
+++ charts_site/js/base.js
@@ -36,6 +36,8 @@ function get_api_url_() {
     return result;
 }
 
+var addressPoints = [];
+
 function fill_chart_() {
     var api_url = get_api_url_();
 
@@ -50,7 +52,18 @@ function fill_chart_() {
                 }
                 $('#id_count_users_head').html(
                     '<p>' +
-                    'Ниже представлены данные о самочувствии ' +
+                    'Ниже представлены данные ' +
+
+                    (data.symptoms ?
+                        'о ' +
+                        '<b><big>' +
+                        data.symptoms +
+                        '</big></b>' +
+                        ' сообщениях ' :
+                        ''
+                    )+
+
+                    'о самочувствии ' +
                     '<b><big>' +
                     data.users +
                     '</big></b> ' +
@@ -98,8 +111,57 @@ function fill_chart_() {
                     "<img style='border:1px solid black;' src='data:image/png;base64," + data.legend + "'>"
                 );
             }
+
+            for (var i = 0; i < data.points.length; i++) {
+                addressPoints.push(data.points[i]);
+            }
+
+            var tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                    maxZoom: 20,
+                    // attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Points &copy 2012 LINZ'
+                }),
+                latlng = L.latLng(data.lat_avg, data.lng_avg);
+
+            var map = L.map('map', { center: latlng, zoom: 4, layers: [tiles] });
+
+            var progress = document.getElementById('progress');
+            var progressBar = document.getElementById('progress-bar');
+
+            var markers = L.markerClusterGroup({ chunkedLoading: true, chunkProgress: updateProgressBar });
+
+            var markerList = [];
+
+            //console.log('start creating markers: ' + window.performance.now());
+
+            for (var i = 0; i < addressPoints.length; i++) {
+                var a = addressPoints[i];
+                var title = a[2];
+                var marker = L.marker(L.latLng(a[0], a[1]), { title: title });
+                marker.bindPopup(title);
+                markerList.push(marker);
+            }
+
+            //console.log('start clustering: ' + window.performance.now());
+
+            markers.addLayers(markerList);
+            map.addLayer(markers);
+
+            //console.log('end clustering: ' + window.performance.now());
         }
     });
+}
+
+function updateProgressBar(processed, total, elapsed, layersArray) {
+    if (elapsed > 1000) {
+        // if it takes more than a second to load, display the progress bar:
+        progress.style.display = 'block';
+        progressBar.style.width = Math.round(processed/total*100) + '%';
+    }
+
+    if (processed === total) {
+        // all markers processed - hide the progress bar:
+        progress.style.display = 'none';
+    }
 }
 
 $(function() {
