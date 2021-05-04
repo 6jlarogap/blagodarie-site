5301bbfa93882b1d3b445f9ab760f40a8e011e7f
Kirill Kowalchuck <kirill.kowa@gmail.com>
2021-05-04 09:44:04 +0300

    mail-message-fix

--- blagodarie/code.js
+++ blagodarie/code.js
@@ -215,7 +215,9 @@ deleteCookie("","set_trust", "set_mistrust");
 		event.preventDefault();
 		if (button.id == "confrim-mail") {
 			var link = document.getElementById("mailLink");
-			link.href = `mailto:${document.getElementById("mailInput").value}?body="${window.location.href}"`;
+			var message = window.location.href;
+			message.replace('?', '%3f');
+			link.href = `mailto:${document.getElementById("mailInput").value}?body=${message}`;
 			link.click();
 		} else if (button.id == "confrim-sms") {
 			var link = document.getElementById("smsLink");
