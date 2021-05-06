471a38390976f0245d07a825726700ca4e611235
Kirill Kowalchuck <kirill.kowa@gmail.com>
2021-05-06 07:53:28 +0300

    menu-positions-change

--- blagodarie/code.js
+++ blagodarie/code.js
@@ -751,35 +751,35 @@ d3.json(apiUrl)
 			d.fy = height / 2;
 			break;
 		case WISHES_ROOT_ID:
-			d.fx = width / 2 + 400;
-			d.fy = height / 2 + 300;
+			d.fx = width / 2 + 500;
+			d.fy = height / 2 + 200;
 			break;
 		case KEYS_ROOT_ID:
-			d.fx = width / 2 + 400;
-			d.fy = height / 2 - 300;
+			d.fx = width / 2 + 500;
+			d.fy = height / 2 - 200;
 			break;
 		case ABILITIES_ROOT_ID:
-			d.fx = width / 2 + 400;
+			d.fx = width / 2 + 500;
 			d.fy = height / 2;
 			break;
 		case SHARE_ID:
-			d.fx = width / 2 + 200;
-			d.fy = height / 2 - 300;
-			break;
-		case FILTER_ID:
 			d.fx = width / 2 + 300;
 			d.fy = height / 2 - 300;
 			break;
+		case FILTER_ID:
+			d.fx = width / 2 + 400;
+			d.fy = height / 2 - 300;
+			break;
 		case OPTIONS_ID:
-			d.fx = width / 2;
+			d.fx = width / 2 - 400;
 			d.fy = height / 2 - 300;	
 			break;
 		case INVITE_ID:
-				d.fx = width / 2 - 100;
+				d.fx = width / 2 + 200;
 				d.fy = height / 2 - 300;	
 				break;
 		case HOME_ID:
-			d.fx = width / 2 + 100;
+			d.fx = width / 2 - 300;
 			d.fy = height / 2 - 300;
 			break;
 		case TRUST_ID:
