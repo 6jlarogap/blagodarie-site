f52c7402b7c62b67a75b6bb0230a2ca3dd40ef00
NikitaLastivka <84903670+NikitaLastivka@users.noreply.github.com>
2021-07-02 11:36:56 +0300

    Update code.js

--- blagodarie/code.js
+++ blagodarie/code.js
@@ -635,7 +635,8 @@ d3.json(apiUrl)
 		//добавить вершину доверие/недоверие
 		nodes.push({
 			id: TRUST_ID,
-			text: "Доверие" + response.thanks_count,
+			text: "Доверие",
+			p: response.thanks_count,
 			image: !isConnection ? inactiveTrust : isTrust ? activeTrust : inactiveTrust,
 			nodeType: NODE_TYPES.TRUST
 		});
