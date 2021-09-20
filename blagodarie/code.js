1afffeb6fec78408b61589edd4b0255b78f19f3e
NikitaLastivka <84903670+NikitaLastivka@users.noreply.github.com>
2021-09-20 23:59:17 +0300

    Update code.js

--- blagodarie/code.js
+++ blagodarie/code.js
@@ -564,7 +564,7 @@ d3.json(apiUrl)
 		if (!nodes.some(user => user.id == d.uuid)) {
 			nodes.push ({
 				id: d.uuid,
-				text: (d.first_name + " " + d.last_name),
+				text: (d.first_name + " " + d.last_name + d.ability),
 				image: d.photo == '' ? `${settings.url}images/default_avatar.png` : d.photo,
 				nodeType: (d.uuid == userIdFrom ? NODE_TYPES.USER : localStorage.getItem("filter") != null && !(d.first_name + " " + d.last_name).toLowerCase().includes(localStorage.getItem("filter").toLowerCase()) ? NODE_TYPES.FILTERED : NODE_TYPES.FRIEND)
 			});
