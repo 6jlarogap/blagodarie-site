d5785de863ed882755449e0c44a3502a381c7cfb
NikitaLastivka <84903670+NikitaLastivka@users.noreply.github.com>
2021-09-21 22:33:54 +0300

    Update code.js

--- blagodarie/code.js
+++ blagodarie/code.js
@@ -568,12 +568,7 @@ d3.json(apiUrl)
 				image: d.photo == '' ? `${settings.url}images/default_avatar.png` : d.photo,
 				nodeType: (d.uuid == userIdFrom ? NODE_TYPES.USER : localStorage.getItem("filter") != null && !(d.first_name + " " + d.last_name).toLowerCase().includes(localStorage.getItem("filter").toLowerCase()) ? NODE_TYPES.FILTERED : NODE_TYPES.FRIEND)
 			});
-			nodes.push({
-				id: `ability_${d.uuid}`,
-				text: d.text,
-				image: `${settings.url}images/ability.png`,
-				nodeType: NODE_TYPES.ABILITY
-			});
+			
 		}
 	});
 
