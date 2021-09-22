ec98b5e57f787089b9f2c0aadbe8a68072b28c3e
NikitaLastivka <84903670+NikitaLastivka@users.noreply.github.com>
2021-09-22 07:53:36 +0300

    Update code.js

--- blagodarie/code.js
+++ blagodarie/code.js
@@ -578,53 +578,20 @@ d3.json(apiUrl)
 		nodes.push(PROFILE);
 	}
 
-if (localStorage.getItem('filter') != null) {
-	//добавить пользователей в вершины
+
 	
 	
-	async function filter_head(query_inp) {
+	/*async function filter_head(query_inp) {
 	const responseee = await fetch(`${settings.api}/api/getstats/user_connections_graph?query=${query_inp}`, {
 		method: "GET",
 		headers: {
 			"Authorization": 'Token ' + getCookie("auth_token")
 		}
 	}).then(data => data.json());
-		console.log(responseee)
-		responseee.users.forEach(function(d){
-		
-			if(d.ability === null){
-			nodes.push ({
-				id: d.uuid,
-				text: (d.first_name + " " + d.last_name + " " + " "),
-				image: d.photo == '' ? `${settings.url}images/default_avatar.png` : d.photo,
-				nodeType: (d.uuid == userIdFrom ? NODE_TYPES.USER : localStorage.getItem("filter") != null && !(d.first_name + " " + d.last_name).toLowerCase().includes(localStorage.getItem("filter").toLowerCase()) ? NODE_TYPES.FILTERED : NODE_TYPES.FRIEND)
-			});
-			}else{
-				nodes.push ({
-				id: d.uuid,
-				text: (d.first_name + " " + d.last_name),
-				tabil: (d.ability),
-				image: d.photo == '' ? `${settings.url}images/default_avatar.png` : d.photo,
-				nodeType: (d.uuid == userIdFrom ? NODE_TYPES.USER : localStorage.getItem("filter") != null && !(d.first_name + " " + d.last_name).toLowerCase().includes(localStorage.getItem("filter").toLowerCase()) ? NODE_TYPES.FILTERED : NODE_TYPES.FRIEND)
-			});
-			}
-			
-			
-		
-	});
+		console.log(responseee)*/
 		
 		
 		
-	};
-	
-	
-filter_head(localStorage.getItem('filter'))	
-	
-	
-	
-
-}else{
-console.log(localStorage.getItem('filter'))			
 	//добавить пользователей в вершины
 	data.users.forEach(function(d){
 		if (!nodes.some(user => user.id == d.uuid)) {
@@ -651,7 +618,7 @@ console.log(localStorage.getItem('filter'))
 				
 				
 				
-}
+
 	
 	
 	
