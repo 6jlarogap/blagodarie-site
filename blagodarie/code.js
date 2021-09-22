bac26c260e367957485b4c9907eac756caf57294
NikitaLastivka <84903670+NikitaLastivka@users.noreply.github.com>
2021-09-22 04:39:52 +0300

    Update code.js

--- blagodarie/code.js
+++ blagodarie/code.js
@@ -574,6 +574,7 @@ d3.json(apiUrl)
 				nodes.push ({
 				id: d.uuid,
 				text: (d.first_name + " " + d.last_name + "\n " + d.ability),
+				tspan: (d.ability),
 				image: d.photo == '' ? `${settings.url}images/default_avatar.png` : d.photo,
 				nodeType: (d.uuid == userIdFrom ? NODE_TYPES.USER : localStorage.getItem("filter") != null && !(d.first_name + " " + d.last_name).toLowerCase().includes(localStorage.getItem("filter").toLowerCase()) ? NODE_TYPES.FILTERED : NODE_TYPES.FRIEND)
 			});
