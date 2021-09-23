2153f65c0d62b214fbdba86550a849014c22689d
NikitaLastivka <84903670+NikitaLastivka@users.noreply.github.com>
2021-09-23 04:49:11 +0300

    Update code.js

--- blagodarie/code.js
+++ blagodarie/code.js
@@ -574,11 +574,13 @@ function nextPage(){
 		}
 		else if(localStorage.getItem('cur_page') < 99){
 			let min_int = window.location.search.slice(1, -2);
+			console.log(min_int);
 			+min_int;
+			console.log(min_int);
 			min_int++;
 			console.log(min_int);
-			localStorage.setItem('cur_page', min_int);
-			window.location.assign(window.location.origin + window.location.pathname + window.location.search.slice(1, -1) + (+localStorage.getItem('cur_page')));
+			//localStorage.setItem('cur_page', min_int);
+			//window.location.assign(window.location.origin + window.location.pathname + window.location.search.slice(1, -1) + (+localStorage.getItem('cur_page')));
 		}
 	}
 }
