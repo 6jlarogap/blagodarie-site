b564362625735648ab9258c3d974a5236097f861
NikitaLastivka <84903670+NikitaLastivka@users.noreply.github.com>
2021-09-23 04:04:35 +0300

    Update code.js

--- blagodarie/code.js
+++ blagodarie/code.js
@@ -577,7 +577,7 @@ function prevPage(){
 		let new_int = window.location.search.slice(-1);
 		+new_int;
 		new_int--;
-		window.location.assign(window.location.origin + window.location.pathname + window.location.search.slice(0, -1) + new_int);
+		window.location.assign(window.location.origin + window.location.pathname + window.location.search.slice(0, -1) + (+localStorage.getItem('cur_page')-1));
 	}
 }
 
