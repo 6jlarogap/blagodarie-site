43097f6370e94a6c17a84fce87af30fe617dc41d
NikitaLastivka <84903670+NikitaLastivka@users.noreply.github.com>
2021-09-24 03:46:20 +0300

    Update code.js

--- blagodarie/code.js
+++ blagodarie/code.js
@@ -638,16 +638,20 @@ document.querySelector('#page').innerHTML = current_page2.replace('?page=', '');
        function prevPage(){
 	        current_page--;
 	        if(window.location.href.includes('page=')){
-                	if(localStorage.getItem('cur_page') < 3){
+                	if(localStorage.getItem('cur_page') < 3 && window.location.href.includes('?page=2')){
                     		window.location.assign(window.location.origin + window.location.pathname + window.location.search.replace('?page=2', ''))
-                	}else if(localStorage.getItem('cur_page') < 10){
+                	}
+                	else if(localStorage.getItem('cur_page') < 3 && window.location.href.includes('&page=2')){
+                    		window.location.assign(window.location.origin + window.location.pathname + window.location.search.replace('&page=2', ''))
+                	}
+		        else if(localStorage.getItem('cur_page') < 10){
 			        let new_int = window.location.search.slice(-1);
 			        +new_int;
 			        new_int--;
 			        localStorage.setItem('cur_page', new_int);
 			        window.location.assign(window.location.origin + window.location.pathname + window.location.search.slice(0, -1) + (+localStorage.getItem('cur_page')));
 		        }else if(localStorage.getItem('cur_page') < 99){
-                    		let new_int = window.location.search.slice(-2);
+                    let new_int = window.location.search.slice(-2);
 			        +new_int;
 			        new_int--;
 			        localStorage.setItem('cur_page', new_int);
