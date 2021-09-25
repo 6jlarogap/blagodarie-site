a6ad5ef38c33bbec0cb2079e1fe57a291b32b797
NikitaLastivka <84903670+NikitaLastivka@users.noreply.github.com>
2021-09-26 00:52:56 +0300

    Update code.js

--- blagodarie/code.js
+++ blagodarie/code.js
@@ -647,17 +647,18 @@ function show_smart_map(lat, long){
 	mapid.style.left = '0';
 	mapid = L.map('mapid').setView([51.505, -0.09], 13);
 	mapid.style.position = 'absolute!important';
-}
-
-
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+	
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={pk.eyJ1IjoibmlraXRhbGFzdCIsImEiOiJja3UwYmtnbjYwOWo0MnZvMTJ3ZTRiY3ZhIn0.5YnAsUvxjkv-oyTUmD-Kxw}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
-    accessToken: 'your.mapbox.access.token'
+    accessToken: 'pk.eyJ1IjoibmlraXRhbGFzdCIsImEiOiJja3UwYmtnbjYwOWo0MnZvMTJ3ZTRiY3ZhIn0.5YnAsUvxjkv-oyTUmD-Kxw'
 }).addTo(mapid);
+}
+
+
 
 
 
