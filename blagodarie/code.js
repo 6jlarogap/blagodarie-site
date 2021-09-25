d7592411838c657ccf020fae75e2940c77807ca6
NikitaLastivka <84903670+NikitaLastivka@users.noreply.github.com>
2021-09-26 00:50:59 +0300

    Update code.js

--- blagodarie/code.js
+++ blagodarie/code.js
@@ -649,6 +649,18 @@ function show_smart_map(lat, long){
 	mapid.style.position = 'absolute!important';
 }
 
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'your.mapbox.access.token'
+}).addTo(mapid);
+
+
+
        
         if (!window.location.href.includes('page=')) {
 		let btn_prev_n = document.querySelector('#btn_prev');
