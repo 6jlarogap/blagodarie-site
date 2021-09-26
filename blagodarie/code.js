4d5e45b8dded7f8891053ed14d9e04a1bb566b48
NikitaLastivka <84903670+NikitaLastivka@users.noreply.github.com>
2021-09-26 04:52:27 +0300

    Update code.js

--- blagodarie/code.js
+++ blagodarie/code.js
@@ -631,8 +631,8 @@ function show_smart_map(lati, long){
 	var marker = L.marker([lati, long]).addTo(mapid);
 	function onMapClick(e) {
     		marker
-        		.setLatLng(e.latlng)
-        		.getLatLng(lati, long)
+        		.setLatLng([e.lat, e.lon])
+        		
         		
 		
 		console.log('after ' + lati + long);
