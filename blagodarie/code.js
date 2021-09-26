99a154f2a6456c8039cf2c1e2a0a8aa12f73f1c9
NikitaLastivka <84903670+NikitaLastivka@users.noreply.github.com>
2021-09-26 08:54:33 +0300

    Update code.js

--- blagodarie/code.js
+++ blagodarie/code.js
@@ -686,7 +686,7 @@ document.querySelector(".mapid_clean").addEventListener("click", async () => {
 		btn_prev_n.style.pointerEvents = 'none';
             localStorage.setItem('item_plus', 0);
 		let selected_val = +localStorage.getItem('selected_val');
-		localStorage.setItem('selected_val', selected_val);
+		localStorage.setItem('selected_val', 25);
 		document.querySelector('.pagination_select').style.display = 'block';
 		document.querySelector('.pagination_count').innerHTML = localStorage.getItem('selected_val');
             var apiUrl = `${settings.api}api/getstats/user_connections_graph?from=0&number=${localStorage.getItem('selected_val')}`;
