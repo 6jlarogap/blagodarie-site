56d4d058a035ca13097bc363d395a6daa74132ec
NikitaLastivka <84903670+NikitaLastivka@users.noreply.github.com>
2021-09-28 14:07:40 +0300

    Update code.js

--- blagodarie/code.js
+++ blagodarie/code.js
@@ -1320,7 +1320,7 @@ function show_map_style(){
 			var new_marker = new L.marker([map_users[i].user_latitude, map_users[i].user_longitude], {icon: myIcon})
 			.addTo(new_map)
 			new_marker.addEventListener('click', ()=>{
-				window.location.assign(window.location.href + map_users[i].user_uuid)
+				window.open(window.location.origin + '/profile/?id=' + map_users[i].user_uuid);
 			})
 			
 		}
