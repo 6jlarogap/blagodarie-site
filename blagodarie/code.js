66fe70024138ff9801635f4cf5e9f7be8f98c8bc
NikitaLastivka <MrSuperskuy@gmail.com>
2021-10-05 09:41:47 +0300

    Example commit

--- blagodarie/code.js
+++ blagodarie/code.js
@@ -1323,26 +1323,9 @@ d3.json(apiUrl)
 	initializeSimulation();
 });
 
-// функ удаления параметров из адресной строки
 
-function removeParam(key, sourceURL) {
-  var splitUrl = sourceURL.split('?'),
-    rtn = splitUrl[0],
-    param,
-    params_arr = [],
-    queryString = (sourceURL.indexOf("?") !== -1) ? splitUrl[1] : '';
-  if (queryString !== '') {
-    params_arr = queryString.split('&');
-    for (var i = params_arr.length - 1; i >= 0; i -= 1) {
-      param = params_arr[i].split('=')[0];
-      if (param === key) {
-        params_arr.splice(i, 1);
-      }
-    }
-    rtn = rtn + '?' + params_arr.join('&');
-  }
-  return rtn;
-}
+
+
 
 
 
