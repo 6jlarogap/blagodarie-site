307bbd28eeb6e1792e522aa1a042971e7d0d4488
NikitaLastivka <MrSuperskuy@gmail.com>
2021-10-13 02:13:06 +0300

    Ex

--- blagodarie/code.js
+++ blagodarie/code.js
@@ -1233,7 +1233,7 @@ d3.json(apiUrl)
 	simulation.force("link", d3.forceLink(links).id(d => d.id).distance(150).links(links)); //distance(150)
 	simulation.force("charge", d3.forceManyBody().strength(0.5));
 	//simulation.force("center", d3.forceCenter(width / 2, height / 2))
-	simulation.force("collide", d3.forceCollide().strength(0.4).radius(80).iterations(0));//radius 80  strength(0.6)
+	simulation.force("collide", d3.forceCollide().strength(0.4).radius(80).iterations(0.2));//radius 80  strength(0.6)
 	simulation.force("x", d3.forceX(width / 2).strength(0.5)); //strength(0.2))
 	simulation.force("y", d3.forceY(height / 2).strength(0.5)); // strength(0.2))
 	
