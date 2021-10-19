45d1a4763c499fd5a38eafc66c928b56022e2d08
NikitaLastivka <MrSuperskuy@gmail.com>
2021-10-19 05:23:39 +0300

    Example commit

--- blagodarie/code.js
+++ blagodarie/code.js
@@ -807,7 +807,7 @@ var url = new URL(link);
                 apiUrl = `${settings.api}api/profile_graph?from=${url.searchParams.get('f')}&number=${url.searchParams.get('q')}&uuid=` + userIdFrom;
                 //console.log(apiUrl);
             } else if (localStorage.getItem('filter') != null) {
-                apiUrl = `${settings.api}api/profile_graph?uuid=${userIdTo}&from=${url.searchParams.get('f')}&number=${url.searchParams.get('q')}&query=` + localStorage.getItem('filter');
+                apiUrl = `${settings.api}api/profile_graph?uuid=${getCookie('user_uuid')}&from=${url.searchParams.get('f')}&number=${url.searchParams.get('q')}&query=` + localStorage.getItem('filter');
                 //console.log(apiUrl);
             }
 		console.log(apiUrl);
