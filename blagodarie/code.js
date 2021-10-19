3d635311f8982e63942473c83cf2be23422bae0e
NikitaLastivka <MrSuperskuy@gmail.com>
2021-10-19 03:46:56 +0300

    Example commit

--- blagodarie/code.js
+++ blagodarie/code.js
@@ -1214,11 +1214,11 @@ d3.json(apiUrl)
 			break;
 		case TRUST_ID:
 			d.fx = width / 2 + 50;
-			d.fy = height / 2 + 100;
+			d.fy = width<900 ? height/2+60 : height / 2 + 100;
 			break;
 		case MISTRUST_ID:
 			d.fx = width / 2 - 50;
-			d.fy = width<900 ? height/2+10 : height / 2 + 100;
+			d.fy = width<900 ? height/2+60 : height / 2 + 100;
 			break;
 		case AUTH_ID:
 			if (!userIdFrom) {
