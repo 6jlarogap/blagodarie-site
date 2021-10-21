daa9cd6f4d8654cfbeadf9e19b0db40d7391e563
NikitaLastivka <MrSuperskuy@gmail.com>
2021-10-22 02:26:28 +0300

    Example commit

--- blagodarie/code.js
+++ blagodarie/code.js
@@ -605,7 +605,7 @@ var userIdTo = url.searchParams.get("userIdTo");
 var fromApp = url.searchParams.get("from_app");
 let my_family_profiles = document.querySelector('#my_family_profiles');
 my_family_profiles.addEventListener('click', function(){
-	window.location.href = url.origin + '/profiles';
+	window.location.href = url.origin + '/profiles?id=' + url.searchParams.get('id');
 })
 
 
