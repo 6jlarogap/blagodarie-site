2411b282fa4c869a09a5a6db05b37eba3a705083
NikitaLastivka <MrSuperskuy@gmail.com>
2021-10-22 03:03:43 +0300

    Example commit

--- blagodarie/profiles/code.js
+++ blagodarie/profiles/code.js
@@ -10,7 +10,7 @@ function getCookie(name) {
 
 
 let user_table_body = document.querySelector('#user_table_body');
-let tr = document.createElement('tr');
+
 async function myProfilesinfo() {
 		const response = await fetch(`https://api.dev.blagodarie.org/api/profile_genesis?uuid=` + url.searchParams.get('id'), {
 		method: "GET",
@@ -21,6 +21,7 @@ async function myProfilesinfo() {
 	console.log(response);
 	for(let i = 0; i<response.users.length; i++){
 		console.log(response.users[i]);
+		let tr = document.createElement('tr');
 		tr.innerHTML = "<td>1</td><td>2</td><td>3</td>";
 		user_table_body.append(tr); 
 	}
