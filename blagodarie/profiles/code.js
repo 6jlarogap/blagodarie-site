1fbea1e3ebdf5464939998e692805a0cbe9fcdeb
NikitaLastivka <MrSuperskuy@gmail.com>
2021-10-24 08:06:43 +0300

    Example commit

--- blagodarie/profiles/code.js
+++ blagodarie/profiles/code.js
@@ -75,6 +75,7 @@ function user_changed_info(id, last_name, first_name, middle_name, usr_photo){
 	add_user_profile_overbottom.addEventListener('click', function(){
 		var formdata = new FormData();
 		formdata.append("first_name", user_profile_name_inp.value);
+		formdata.append("last_name", user_profile_surname_inp.value);
 		async function add_gen(){
 		const response = await fetch(`${settings.api}api/profile`, {
 		method: "POST",
