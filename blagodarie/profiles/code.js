cf74e8e9afe04fb5edf9aef1ecfea4ad9a2d654c
NikitaLastivka <MrSuperskuy@gmail.com>
2021-10-24 07:46:57 +0300

    Example commit

--- blagodarie/profiles/code.js
+++ blagodarie/profiles/code.js
@@ -70,7 +70,22 @@ function user_changed_info(id, last_name, first_name, middle_name, usr_photo){
 	
 	add_user_profile_overbottom.addEventListener('click', function(){
 		await add_gen();
-		async function add_gen(){
+	});
+	
+	
+	
+	
+	//закрыть попап
+	add_user_profile_close_popup.addEventListener('click', function(){
+		add_user_profile_container.style.display = "none";
+	})
+	
+}
+
+
+
+
+async function add_gen(){
 		const response = await fetch(`${settings.api}api/profile`, {
 		method: "PUT",
 		headers: {
@@ -82,18 +97,6 @@ function user_changed_info(id, last_name, first_name, middle_name, usr_photo){
 		console.log(response);
 		
 		}
-		
-		
-		
-		
-	});
-	
-	//закрыть попап
-	add_user_profile_close_popup.addEventListener('click', function(){
-		add_user_profile_container.style.display = "none";
-	})
-	
-}
 
 
 
