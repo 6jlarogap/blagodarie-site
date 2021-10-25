520bf9440065005f3c267865b130ec0e688b43b1
NikitaLastivka <MrSuperskuy@gmail.com>
2021-10-25 03:03:03 +0300

    Example commit

--- blagodarie/profiles/code.js
+++ blagodarie/profiles/code.js
@@ -60,7 +60,74 @@ function user_changed_info(id, last_name, first_name, middle_name, usr_photo, do
 	let add_user_profile_dd = document.querySelector('.add_user_profile_dd');
 	let add_user_profile_mother_input = document.querySelector('.add_user_profile_mother_input');
 	
+	//обрезка файлов
+	var bs_modal = $('#modal');
+    var image = document.getElementById('image');
+    var cropper,reader,file;
+   
+
+    $("body").on("change", ".image", function(e) {
+        var files = e.target.files;
+        var done = function(url) {
+            image.src = url;
+            bs_modal.modal('show');
+        };
+
+
+        if (files && files.length > 0) {
+            file = files[0];
+
+            if (URL) {
+                done(URL.createObjectURL(file));
+            } else if (FileReader) {
+                reader = new FileReader();
+                reader.onload = function(e) {
+                    done(reader.result);
+                };
+                reader.readAsDataURL(file);
+            }
+        }
+    });
+
+    bs_modal.on('shown.bs.modal', function() {
+        cropper = new Cropper(image, {
+            aspectRatio: 1,
+            viewMode: 3,
+            preview: '.preview'
+        });
+    }).on('hidden.bs.modal', function() {
+        cropper.destroy();
+        cropper = null;
+    });
+
+    $("#crop").click(function() {
+        canvas = cropper.getCroppedCanvas({
+            width: 160,
+            height: 160,
+        });
+
+        canvas.toBlob(function(blob) {
+            url = URL.createObjectURL(blob);
+            var reader = new FileReader();
+            reader.readAsDataURL(blob);
+            reader.onloadend = function() {
+                var base64data = reader.result;
+
+                $.ajax({
+                    type: "PUT",
+                    dataType: "json",
+                    url: `${settings.api}api/profile`,
+                    data: {image: base64data},
+                    success: function(data) { 
+                        bs_modal.modal('hide');
+                        alert("success upload image");
+                    }
+                });
+            };
+        });
+    });
 	
+	//конец
 	
 	user_profile_surname_inp.value = last_name;
 	user_profile_name_inp.value = first_name;
