b4de0a58f81723809cae1214c87f1d0a8eb2e3bb
NikitaLastivka <MrSuperskuy@gmail.com>
2021-10-27 05:10:13 +0300

    Example commit

--- blagodarie/profiles/code.js
+++ blagodarie/profiles/code.js
@@ -277,6 +277,7 @@ function user_changed_info(id, last_name, first_name, middle_name, usr_photo, do
 				}
 			}
 			if(response.connections.length == users_resp.length){
+				if()
 				console.log(users_resp);
 				add_user_parents(8, add_user_profile_mother_input.value)
 			}
