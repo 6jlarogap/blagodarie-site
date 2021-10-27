2474fa56ac57c3494fc3bd45624e6ded28bb9a0c
NikitaLastivka <MrSuperskuy@gmail.com>
2021-10-27 04:39:17 +0300

    Example commit

--- blagodarie/profiles/code.js
+++ blagodarie/profiles/code.js
@@ -231,7 +231,8 @@ function user_changed_info(id, last_name, first_name, middle_name, usr_photo, do
 		headers: {
 			"Authorization": 'Token ' + getCookie("auth_token")
 		}
-}).then(data => data.json());
+}).then(data => data.json())
+	.catch(error => console.log('error', error));		
 			//var b;
 			if(add_user_profile_mother_input.value.includes('id')){
 				let str = add_user_profile_mother_input.value;
