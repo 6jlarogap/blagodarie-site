e4976fa91372c88e4579a9a034b71f5081229b4e
NikitaLastivka <MrSuperskuy@gmail.com>
2021-10-29 00:52:07 +0300

    Example commit

--- blagodarie/profiles/code.js
+++ blagodarie/profiles/code.js
@@ -80,7 +80,7 @@ function user_changed_info(id, last_name, first_name, middle_name, usr_photo, do
 	
 	
 	//обрезка файлов
-	window.addEventListener('DOMContentLoaded', function () {
+
 	var bs_modal = $('#modal');
     var image = document.getElementById('image');
     var cropper,reader,file;
@@ -195,7 +195,7 @@ function user_changed_info(id, last_name, first_name, middle_name, usr_photo, do
             };
         });
     });
-});
+
 	
 	//обезличивание 
 	
