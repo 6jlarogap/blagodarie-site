0a262c14803bbac7a74e9e4a4387beef2f92ef39
NikitaLastivka <MrSuperskuy@gmail.com>
2021-10-29 02:46:10 +0300

    Example commit

--- blagodarie/gen/code.js
+++ blagodarie/gen/code.js
@@ -1241,10 +1241,11 @@ d3.json(apiUrl)
 	simulation.force("link", d3.forceLink(links).id(d => d.id).distance(30).links(links)); //distance(150)
 	simulation.force("link", d3.forceLink(links_parent).id(d => d.id).distance(30).links(links_parent)); //distance(150)
 	simulation.force("charge", d3.forceManyBody().strength(-400)); //0.5
-	//simulation.force("center", d3.forceCenter(width / 2, height / 2))
-	simulation.force("collide", d3.forceCollide().strength(0.4).radius(45).iterations(1));//radius 55  strength(0.6)
+	/*simulation.force("collide", d3.forceCollide().strength(0.4).radius(45).iterations(1));//radius 55  strength(0.6)
 	simulation.force("x", d3.forceX(width / 2).strength(0.5)); //strength(0.2))
-	simulation.force("y", d3.forceY(height / 2).strength(0.5)); // strength(0.2))
+	simulation.force("y", d3.forceY(height / 2).strength(0.5)); // strength(0.2))*/
+	simulation.force("x", d3.forceX())
+    simulation.force("y", d3.forceY());
 	}	
 	
 	else{
@@ -1252,10 +1253,11 @@ d3.json(apiUrl)
 	simulation.force("link", d3.forceLink(links).id(d => d.id).distance(150).links(links)); //distance(150)
 	simulation.force("link", d3.forceLink(links_parent).id(d => d.id).distance(150).links(links_parent)); //distance(150)
 	simulation.force("charge", d3.forceManyBody().strength(0.5));
-	//simulation.force("center", d3.forceCenter(width / 2, height / 2))
-	simulation.force("collide", d3.forceCollide().strength(0.4).radius(80).iterations(1));//radius 80  strength(0.6)
+	/*simulation.force("collide", d3.forceCollide().strength(0.4).radius(80).iterations(1));//radius 80  strength(0.6)
 	simulation.force("x", d3.forceX(width / 2).strength(0.5)); //strength(0.2))
-	simulation.force("y", d3.forceY(height / 2).strength(0.5)); // strength(0.2))
+	simulation.force("y", d3.forceY(height / 2).strength(0.5)); // strength(0.2))*/
+	simulation.force("x", d3.forceX())
+    simulation.force("y", d3.forceY());
 	}
 
 	initializeDisplay();
