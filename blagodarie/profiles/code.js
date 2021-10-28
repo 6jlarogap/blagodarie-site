c8dbbd848051ea2e9c7994ad8ab84ba619c41f97
NikitaLastivka <MrSuperskuy@gmail.com>
2021-10-28 04:32:48 +0300

    Example commit

--- blagodarie/profiles/code.js
+++ blagodarie/profiles/code.js
@@ -182,10 +182,8 @@ function user_changed_info(id, last_name, first_name, middle_name, usr_photo, do
 	user_profile_surname_inp.value = last_name;
 	user_profile_name_inp.value = first_name;
 	user_profile_middlename_inp.value = middle_name;
-	const [year2, month2, day2] = dob.split('-');
-	const [year3, month3, day3] = dod.split('-');
-	add_user_profile_bd.value = `${day2}.${month2}.${year2}`;
-	add_user_profile_dd.value = `${day3}.${month3}.${year3}`;
+	add_user_profile_bd.value = dob;
+	add_user_profile_dd.value = dod;
 	/*add_user_profile_bd.value = dob;
 	add_user_profile_dd.value = dod;*/
 	add_user_profile_photo.setAttribute('src', `${usr_photo == '' ? settings.url+'images/default_avatar.png' : usr_photo}`);
@@ -344,8 +342,7 @@ function user_changed_info(id, last_name, first_name, middle_name, usr_photo, do
 		
 		
 		
-		const [year, month, day] = add_user_profile_bd.value.split('.');
-		const [year1, month1, day1] = add_user_profile_dd.value.split('.');
+	
 		//alert(`${day}-${month}-${year}`)
 		
 		
@@ -354,8 +351,8 @@ function user_changed_info(id, last_name, first_name, middle_name, usr_photo, do
 		formdata.append("first_name", user_profile_name_inp.value);
 		formdata.append("last_name", user_profile_surname_inp.value);
 		formdata.append("middle_name", user_profile_middlename_inp.value);
-		formdata.append("dob", `${day}-${month}-${year}`);
-		formdata.append("dod", `${day}-${month}-${year}`);
+		formdata.append("dob", add_user_profile_bd.value);
+		formdata.append("dod", add_user_profile_dd.value);
 		/*async function add_gen(){
 		const response = await fetch(`${settings.api}api/profile`, {
 		method: "PUT",
