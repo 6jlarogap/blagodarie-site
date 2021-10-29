de696271631b9c57f696fe2b7810b967aae14346
NikitaLastivka <MrSuperskuy@gmail.com>
2021-10-29 03:48:26 +0300

    Example commit

--- blagodarie/gen/code.js
+++ blagodarie/gen/code.js
@@ -1256,7 +1256,7 @@ d3.json(apiUrl)
 	simulation.force("link", d3.forceLink(links).id(d => d.id).distance(150).links(links)); //distance(150)
 	simulation.force("link", d3.forceLink(links_parent).id(d => d.id).distance(150).links(links_parent)); //distance(150)
 	simulation.force("charge", d3.forceManyBody().strength(-400));
-	simulation.force("center", d3.forceCenter(width / 2, height / 2))
+	//simulation.force("center", d3.forceCenter(width / 2, height / 2))
 	//simulation.force("collide", d3.forceCollide().strength(0.4).radius(80).iterations(1));//radius 80  strength(0.6)
 	/*simulation.force("x", d3.forceX(width / 2).strength(0.5)); //strength(0.2))
 	simulation.force("y", d3.forceY(height / 2).strength(0.5)); // strength(0.2))*/
@@ -1647,10 +1647,10 @@ function ticked() {
 		*/
 		var x = (d.x < 30 ? 30 : (d.x > width-30 ? width-30 : d.x));
 		var y = (d.y < 15 && width<900 ? 15 : d.y < 0 ? 0 : (d.y > height-20 && width<900 ? height-20 : d.y > height-70 && width>900 ? height-70 : d.y));
-		/*if (d.nodeType == NODE_TYPES.USER || d.nodeType == NODE_TYPES.PROFILE){
+		if (d.nodeType == NODE_TYPES.USER || d.nodeType == NODE_TYPES.PROFILE){
 			simulation.force("x").x(x);
 			simulation.force("y").y(y);
-		}*/
+		}
 		return `translate(${x},${y})`;
 	});
 	
