ef98e9a81f62a98087e42f8816791ab7a976ec3c
NikitaLastivka <MrSuperskuy@gmail.com>
2021-10-29 10:29:28 +0300

    Example commit

--- blagodarie/gen/code.js
+++ blagodarie/gen/code.js
@@ -1241,8 +1241,8 @@ d3.json(apiUrl)
 	
 	if(width<900){
 		simulation = d3.forceSimulation(nodes);
-	simulation.force("link", d3.forceLink(links).id(d => d.id).distance(50).links(links)); //distance(150)
-	simulation.force("link", d3.forceLink(links_parent).id(d => d.id).distance(50).links(links_parent)); //distance(150)
+	simulation.force("link", d3.forceLink(links).id(d => d.id).distance(70).links(links)); //distance(150)
+	simulation.force("link", d3.forceLink(links_parent).id(d => d.id).distance(70).links(links_parent)); //distance(150)
 	simulation.force("charge", d3.forceManyBody().strength(-400)); //0.5
 	/*simulation.force("collide", d3.forceCollide().strength(0.4).radius(45).iterations(1));//radius 55  strength(0.6)
 	simulation.force("x", d3.forceX(width / 2).strength(0.5)); //strength(0.2))
