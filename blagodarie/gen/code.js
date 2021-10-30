4a67cb2f260d175caf95be993e690fb1704ba663
NikitaLastivka <84903670+NikitaLastivka@users.noreply.github.com>
2021-10-30 13:51:32 +0300

    Update code.js

--- blagodarie/gen/code.js
+++ blagodarie/gen/code.js
@@ -1252,7 +1252,7 @@ d3.json(apiUrl)
 	/*simulation.force("collide", d3.forceCollide().strength(0.4).radius(45).iterations(1));//radius 55  strength(0.6)
 	simulation.force("x", d3.forceX(width / 2).strength(0.5)); //strength(0.2))
 	simulation.force("y", d3.forceY(height / 2).strength(0.5)); // strength(0.2))*/
-	simulation.force("gravity",gravity(0.25))
+	simulation.force("gravity", d3.layout.force().gravity(1))
 	simulation.force("x", d3.forceX())
     simulation.force("y", d3.forceY());
 	}	
@@ -1266,7 +1266,7 @@ d3.json(apiUrl)
 	//simulation.force("collide", d3.forceCollide().strength(0.4).radius(80).iterations(1));//radius 80  strength(0.6)
 	/*simulation.force("x", d3.forceX(width / 2).strength(0.5)); //strength(0.2))
 	simulation.force("y", d3.forceY(height / 2).strength(0.5)); // strength(0.2))*/
-	simulation.force("gravity",gravity(0.25))
+	simulation.force("gravity", d3.layout.force().gravity(1))
 	simulation.force("x", d3.forceX())
     simulation.force("y", d3.forceY());
 	}
