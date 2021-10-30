9ad8dd7b12e60643563588e6a306c30deb56855c
NikitaLastivka <MrSuperskuy@gmail.com>
2021-10-30 17:13:41 +0300

    Example commit

--- blagodarie/gen/code.js
+++ blagodarie/gen/code.js
@@ -1252,6 +1252,7 @@ d3.json(apiUrl)
 	/*simulation.force("collide", d3.forceCollide().strength(0.4).radius(45).iterations(1));//radius 55  strength(0.6)
 	simulation.force("x", d3.forceX(width / 2).strength(0.5)); //strength(0.2))
 	simulation.force("y", d3.forceY(height / 2).strength(0.5)); // strength(0.2))*/
+	simulation.force("center", d3.forceCenter(width / 2, height / 2))
 	simulation.force("x", d3.forceX(width / 2).strength(0.3))
     simulation.force("y", d3.forceY(height/2).strength(0.3));
 	}	
@@ -1265,6 +1266,7 @@ d3.json(apiUrl)
 	//simulation.force("collide", d3.forceCollide().strength(0.4).radius(80).iterations(1));//radius 80  strength(0.6)
 	/*simulation.force("x", d3.forceX(width / 2).strength(0.5)); //strength(0.2))
 	simulation.force("y", d3.forceY(height / 2).strength(0.5)); // strength(0.2))*/
+	simulation.force("center", d3.forceCenter(width / 2, height / 2))
 	simulation.force("x", d3.forceX(width / 2).strength(0.3))
     simulation.force("y", d3.forceY(height/2).strength(0.3));
 	}
