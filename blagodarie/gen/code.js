5d1fa61476825f0ced9bc8dc79266c928b1eccd6
6jlarogap <62824780+6jlarogap@users.noreply.github.com>
2021-10-30 21:38:14 +0300

    Update code.js

--- blagodarie/gen/code.js
+++ blagodarie/gen/code.js
@@ -1244,27 +1244,27 @@ d3.json(apiUrl)
 		}
 	});
 	
-	if(width<900){
-	simulation = d3.forceSimulation(nodes);
-	simulation.force("link", d3.forceLink(links).id(d => d.id).distance(70).links(links)); //distance(150)
-	simulation.force("link", d3.forceLink(links_parent).id(d => d.id).distance(70).links(links_parent)); //distance(150)
-	simulation.force("center", d3.forceCenter(width / 2, height / 2))
-	simulation.force("collide", d3.forceCollide().strength(0.1).radius(90).iterations(1))
-		
-/*		
+	if(width<900){		
 		simulation = d3.forceSimulation(nodes);
 	simulation.force("link", d3.forceLink(links).id(d => d.id).distance(70).links(links)); //distance(150)
 	simulation.force("link", d3.forceLink(links_parent).id(d => d.id).distance(70).links(links_parent)); //distance(150)
 	simulation.force("charge", d3.forceManyBody().strength(10)) //0.5
-	/*simulation.force("collide", d3.forceCollide().strength(0.4).radius(45).iterations(1));//radius 55  strength(0.6)*/
+//	simulation.force("collide", d3.forceCollide().strength(0.4).radius(45).iterations(1));//radius 55  strength(0.6)
 	simulation.force("center", d3.forceCenter(width / 2, height / 2))
 	simulation.force("collide", d3.forceCollide().strength(0.1).radius(90).iterations(1))
 	simulation.force("x", d3.forceX(width / 2).strength(0.1))
     	simulation.force("y", d3.forceY(height/2).strength(0.1));
-*/
 	}	
 	
 	else{
+		simulation = d3.forceSimulation(nodes);
+		simulation.force("link", d3.forceLink(links).id(d => d.id).distance(150).links(links)); //distance(150)
+		simulation.force("link", d3.forceLink(links_parent).id(d => d.id).distance(150).links(links_parent)); //distance(150)
+		simulation.force("charge", d3.forceManyBody().strength(-400))
+		simulation.force("center", d3.forceCenter(width / 2, height / 2))
+		simulation.force("collide", d3.forceCollide().strength(0.1).radius(80).iterations(1))
+		
+/*		
 	simulation = d3.forceSimulation(nodes);
 	simulation.force("link", d3.forceLink(links).id(d => d.id).distance(150).links(links)); //distance(150)
 	simulation.force("link", d3.forceLink(links_parent).id(d => d.id).distance(150).links(links_parent)); //distance(150)
@@ -1275,6 +1275,7 @@ d3.json(apiUrl)
 	simulation.force("collide", d3.forceCollide().strength(0.1).radius(80).iterations(1))
 	simulation.force("x", d3.forceX(width / 2).strength(0.1))
     	simulation.force("y", d3.forceY(height/2).strength(0.1));
+*/
 	}
 
 	initializeDisplay();
