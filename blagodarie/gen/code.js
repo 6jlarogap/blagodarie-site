7bcd3487b4018bb20da3a8c4c68de4c185c0c8f8
NikitaLastivka <84903670+NikitaLastivka@users.noreply.github.com>
2021-10-30 14:10:45 +0300

    Update code.js

--- blagodarie/gen/code.js
+++ blagodarie/gen/code.js
@@ -1254,7 +1254,6 @@ d3.json(apiUrl)
 	simulation.force("y", d3.forceY(height / 2).strength(0.5)); // strength(0.2))*/
 	simulation.force("x", d3.forceX())
     simulation.force("y", d3.forceY());
-	d3.gravity(1)
 	}	
 	
 	else{
@@ -1268,7 +1267,6 @@ d3.json(apiUrl)
 	simulation.force("y", d3.forceY(height / 2).strength(0.5)); // strength(0.2))*/
 	simulation.force("x", d3.forceX())
     simulation.force("y", d3.forceY());
-	d3.gravity(1)
 	}
 
 	initializeDisplay();
@@ -1285,7 +1283,18 @@ d3.json(apiUrl)
 	
 });
 
-
+var force = d3.layout.force()
+    .nodes(nodes)
+    .links(links)
+    .size([w, h])
+    .linkStrength(0.1)
+    .friction(0.9)
+    .linkDistance(20)
+    .charge(-30)
+    .gravity(0.1)
+    .theta(0.8)
+    .alpha(0.1)
+    .start();
 
 
 
