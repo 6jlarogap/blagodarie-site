36a298bda9cba69d96ffd156ecf832f660d4c619
NikitaLastivka <MrSuperskuy@gmail.com>
2021-11-02 00:31:08 +0200

    Example commit

--- blagodarie/profiles/code.js
+++ blagodarie/profiles/code.js
@@ -573,9 +573,11 @@ if(localStorage.getItem('npuuid')){
 }
 }
 
-
+let gender_value;
 function setGender(gender_val){
+	gender_value = gender_val.value;
 		console.log(gender_val.value);
+		console.log(gender_value);
 	}
 
 
