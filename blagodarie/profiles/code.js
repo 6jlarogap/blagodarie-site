a6e5f1128841b2dff4d622242799d5e30a156400
NikitaLastivka <MrSuperskuy@gmail.com>
2021-11-02 05:53:22 +0200

    Example commit

--- blagodarie/profiles/code.js
+++ blagodarie/profiles/code.js
@@ -964,6 +964,14 @@ var addElement = document.getElementById("addElement");
 var elementAddInput = document.getElementById("elementAddInput");
 var keyTypesBtns = document.getElementById("keyTypesBtns");
 
+
+function uuidv4() {
+	return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+	  var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+	  return v.toString(16);
+	});
+}
+
 rootAddElementMenu.addEventListener("click", () => {
 	elementAddInput.value = "";
 	elementAddInput.id = "elementAddInput";
