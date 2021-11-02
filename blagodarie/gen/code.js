8ae400dda0b697722fe6750e2761940797cf1daf
NikitaLastivka <MrSuperskuy@gmail.com>
2021-11-02 07:34:25 +0200

    Example commit

--- blagodarie/gen/code.js
+++ blagodarie/gen/code.js
@@ -1660,7 +1660,7 @@ function initializeDisplay() {
 						return "#000aff";
 					}
 				} else {
-					return "url(#grad_from_" + d.target.id + "_to_" + d.source.id + ")";
+					return "url(#grad_from_" + d.source.id + "_to_" + d.target.id + ")";
 				}
 			} else {
 				return "#345334";
