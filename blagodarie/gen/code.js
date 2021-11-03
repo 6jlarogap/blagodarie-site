46a7e66d5ec1522f4b0137114a663b039de1e456
NikitaLastivka <MrSuperskuy@gmail.com>
2021-11-03 04:57:59 +0200

    Example commit

--- blagodarie/gen/code.js
+++ blagodarie/gen/code.js
@@ -1028,7 +1028,7 @@ d3.json(apiUrl)
 		var inactiveTrust = `${settings.url}images/trust_inactive.png`;
 		var inactiveMistrust = `${settings.url}images/mistrust_inactive.png`;
 
-		isConnection ? isTrust = data.connections.some(link => link.target == PROFILE.id && link.source == userIdFrom && link.is_trust) : null;
+		isConnection ? isTrust = data.trust_connections.some(link => link.target == PROFILE.id && link.source == userIdFrom && link.is_trust) : null;
 		/*isConnection ? isTrust = data.connections.some(link => link.source == PROFILE.id || link.target == PROFILE.id && link.target == userIdFrom || link.source == userIdFrom && link.is_trust) : null;*/
 		console.log(isTrust)
 		async function count_plus() {
