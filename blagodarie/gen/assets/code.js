63cd464437a4238b0459d16d662b2bcca02be920
6jlarogap <62824780+6jlarogap@users.noreply.github.com>
2021-11-06 15:31:05 +0300

    Update code.js

--- blagodarie/gen/assets/code.js
+++ blagodarie/gen/assets/code.js
@@ -1407,7 +1407,7 @@ const NODE_TYPES = Object.freeze({
 //		simulation.force("link", d3.forceLink(links_parent).id(d => d.id).distance(50).links(links_parent));
 //		simulation.force("charge", d3.forceManyBody().strength(-100))
 //		simulation.force("collide", d3.forceCollide().radius(30));
-		simulation.force("center", d3.forceCenter(width / 3, height / 3))
+		simulation.force("center", d3.forceCenter(width / 2, height / 2));
 	}	
 	initializeDisplay();
 	initializeSimulation();
