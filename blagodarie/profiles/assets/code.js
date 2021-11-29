3ae6a41443a2f666073b8d74b6de361b575384aa
NikitaLastivka <MrSuperskuy@gmail.com>
2021-11-29 06:12:46 +0200

    EX

--- blagodarie/profiles/assets/code.js
+++ blagodarie/profiles/assets/code.js
@@ -77,7 +77,8 @@ function user_changed_info(id, last_name, first_name, middle_name, usr_photo, do
 	let add_user_profile_bd = document.querySelector('.add_user_profile_bd');
 	let add_user_profile_dd = document.querySelector('.add_user_profile_dd');
 	let add_user_profile_mother_input = document.querySelector('.add_user_profile_mother_input');
-	let add_user_profile_father_input = document.querySelector('.add_user_profile_father_input');
+	//let add_user_profile_father_input = document.querySelector('.add_user_profile_father_input');
+	let add_user_profile_father = document.querySelector('#add_user_profile_father');
 	let nophoto_but = document.querySelector('.nophoto_but');
 	let cheked_gend = document.getElementsByName('gender');
 	
