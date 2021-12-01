c07b63016acfb2229a7ba346e1b9b4326f58bab1
NikitaLastivka <MrSuperskuy@gmail.com>
2021-12-01 03:37:15 +0200

    Example

--- blagodarie/profiles/assets/code.js
+++ blagodarie/profiles/assets/code.js
@@ -908,7 +908,7 @@ function show_smart_map(lati, long){
 	if(document.querySelector('#mapid').hasChildNodes()){}
 	else{
 		console.log(response_smat_map);
-		if(response_smat_map[0].user_latitude != null){
+		if(response_smat_map || response_smat_map[0].user_latitude != null){
 			/*let lati = +response_smat_map[0].user_latitude;
 			let long = +response_smat_map[0].user_longitude;*/
 			for(let i=0;i<response_smat_map.length;i++){
