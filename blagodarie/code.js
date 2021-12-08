30894dce41498d73486fd9720a2be6263d9af402
NikitaLastivka <MrSuperskuy@gmail.com>
2021-12-08 08:42:41 +0200

    add

--- blagodarie/code.js
+++ blagodarie/code.js
@@ -885,7 +885,7 @@ var url = new URL(link);
                 //console.log(apiUrl);
 				//
             } else if (localStorage.getItem('filter') != null && isAuth) {
-                apiUrl = `${settings.api}api/profile_graph?uuid=${getCookie('user_uuid')}&from=${url.searchParams.get('f')}&number=${url.searchParams.get('q')}&query=` + localStorage.getItem('filter');
+                apiUrl = `${settings.api}api/profile_graph?uuid=${userIdFrom}&from=${url.searchParams.get('f')}&number=${url.searchParams.get('q')}&query=` + localStorage.getItem('filter');
 				
                 //console.log(apiUrl);
             }
