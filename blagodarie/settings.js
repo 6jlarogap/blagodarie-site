a46d4faf2223243cfb1a8cec29963ac9f471afb9
NikitaLastivka <MrSuperskuy@gmail.com>
2021-12-08 08:59:01 +0200

    add

--- blagodarie/settings.js
+++ blagodarie/settings.js
@@ -15,6 +15,22 @@ const settingSets = [
             redirect_uri: 'https://api.xn--80aabjfzq0au.xn--p1ai/api/oauth/callback/odnoklassniki'
         }
     },
+	{
+        url: 'https://www.xn--80aabjfzq0au.xn--p1ai/',
+        api: 'https://api.blagodarie.org/',
+        bot: 'Blagodarie_bot',
+        yandex: {
+            client_id: '1656d92ee4f8436a8e1de4f5938b0786'
+        },
+        vk: {
+            client_id: '7826626',
+            redirect_uri: 'https://api.xn--80aabjfzq0au.xn--p1ai/api/oauth/callback/vk/'
+        },
+        ok: {
+            client_id: '512000749804',
+            redirect_uri: 'https://api.xn--80aabjfzq0au.xn--p1ai/api/oauth/callback/odnoklassniki'
+        }
+    },
     {
         url: 'https://dev.blagodarie.org/',
         api: 'https://api.dev.blagodarie.org/',
