9b41c0cca56a5f39ab998a7c2169e0f108bda9d8
NikitaLastivka <MrSuperskuy@gmail.com>
2021-12-22 00:11:32 +0200

    add

--- blagodarie/gen/assets/code.js
+++ blagodarie/gen/assets/code.js
@@ -1977,7 +1977,7 @@ async function OnfriendClickFunc(uid, nodeType){
 				console.log(resp_owned_users[i]);
 				clickOnUser.style.display = "none";
 				user_changed_info(uid, resp_owned_users[i].last_name, resp_owned_users[i].first_name, resp_owned_users[i].middle_name, resp_owned_users[i].photo, resp_owned_users[i].dob, resp_owned_users[i].dod, resp_owned_users[i].gender, resp_owned_users[i].latitude, resp_owned_users[i].longitude);
-			
+			return
 			});
 			break;
 		}else{
@@ -2088,7 +2088,7 @@ async function OnfriendClickFunc(uid, nodeType){
 	UserMistrust.style.display = "none";
 	ShortRoad.style.display = "none";
 }
-	return
+	
 }
 
 
