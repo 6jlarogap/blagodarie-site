82a9b7195d819e0089e5010a6e63d64e979e6a8f
NikitaLastivka <MrSuperskuy@gmail.com>
2022-01-11 04:03:48 +0200

    add

--- blagodarie/gen/assets/code.js
+++ blagodarie/gen/assets/code.js
@@ -2062,16 +2062,16 @@ function checker(uid){
 //функция проверки элемента
 
 function checkElemByClass(elem){
-	if(document.querySelector('.' + "'" + elem + "'")==true){
+	if(document.querySelector(`.'${elem}'`)==true){
 		return true
-	}else if(document.querySelector('.' + "'" + elem + "'")==false){
+	}else if(document.querySelector(`.'${elem}'`)==false){
 		return false		 
 	}
 }
 function checkElemById(elem){
-	if(document.querySelector('#' + "'" + elem + "'")==true){
+	if(document.querySelector(`#'${elem}'`)==true){
 		return true
-	}else if(document.querySelector('#' + "'" + elem + "'")==false){
+	}else if(document.querySelector(`#'${elem}'`)==false){
 		return false		 
 	}
 }
