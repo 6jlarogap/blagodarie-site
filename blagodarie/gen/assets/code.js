6ad249921e91bf7c855ea636ed87daa4d70abba7
NikitaLastivka <MrSuperskuy@gmail.com>
2022-01-16 02:50:05 +0200

    add

--- blagodarie/gen/assets/code.js
+++ blagodarie/gen/assets/code.js
@@ -1290,8 +1290,8 @@ if(getCookie("auth_token")=="" || getCookie("auth_token")==false){
       simulation.force("link", d3.forceLink(links).id(d => d.id).distance(30).strength(1))
       .force("link", d3.forceLink(links_parent).id(d => d.id).distance(30).strength(1))
       .force("charge", d3.forceManyBody().strength(-50))
-      .force("x", d3.forceX())
-      .force("y", d3.forceY());
+      /*.force("x", d3.forceX())
+      .force("y", d3.forceY());*/
 		
 		
 		
