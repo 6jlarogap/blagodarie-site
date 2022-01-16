2b249eac52372d067885ac9d26d783f50a09839f
NikitaLastivka <MrSuperskuy@gmail.com>
2022-01-16 05:00:59 +0200

    add

--- blagodarie/gen/assets/code.js
+++ blagodarie/gen/assets/code.js
@@ -45,7 +45,7 @@ const PROFILE = {
 }
 let w = window.innerWidth;
 let h = window.innerHeight;
-var svg = d3.select("#main").attr("viewBox", "30 20 " + w-30 + " " + h-20 )
+var svg = d3.select("#main").attr("viewBox", "60 40 " + w-60 + " " + h-40 )
 			.attr("preserveAspectRatio", "xMidYMid meet");
 var width = +svg.node().getBoundingClientRect().width;
 var height = +svg.node().getBoundingClientRect().height;
