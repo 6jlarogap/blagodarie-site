7a5aac774373bbe4202df64242a11fc98a290bb7
NikitaLastivka <MrSuperskuy@gmail.com>
2022-01-17 07:12:03 +0200

    add

--- blagodarie/gen/assets/code.js
+++ blagodarie/gen/assets/code.js
@@ -1759,11 +1759,10 @@ function initializeDisplay() {
 }
 
 function ticked() {
-	/*node.attr("transform", d => {
-		/*
-		Было
+	node.attr("transform", d => {
+		
 		var x = (d.x < 30 ? 30 : (d.x > width-30 ? width-30 : d.x));
-		var y = (d.y < 15 && width<900 ? 15 : d.y < 0 ? 0 : (d.y > height-20 && width<900 ? height-20 : d.y > height-70 && width>900 ? height-70 : d.y));*/
+		var y = (d.y < 15 && width<900 ? 15 : d.y < 0 ? 0 : (d.y > height-20 && width<900 ? height-20 : d.y > height-70 && width>900 ? height-70 : d.y));
 		
 		
 		
@@ -1774,9 +1773,9 @@ function ticked() {
 		if (d.nodeType == NODE_TYPES.USER || d.nodeType == NODE_TYPES.PROFILE){
 //			simulation.force("x").x(x);
 //			simulation.force("y").y(y);
-		}
+		}*/
 		return `translate(${x},${y})`;
-	});*/
+	});
 	
 	link.selectAll("g")
 		.attr("x1", calcX1)
@@ -1808,48 +1807,6 @@ function ticked() {
 		.attr("y1", calcY1)
 		.attr("x2", calcX2)
 		.attr("y2", calcY2);
-	
-	
-	
-	
-	node.attr("cx", d => d.x)
-        .attr("cy", d => d.y);
-	
-	/*link.selectAll("g")
-        .attr("x1", d => d.source.x)
-        .attr("y1", d => d.source.y)
-        .attr("x2", d => d.target.x)
-        .attr("y2", d => d.target.y);
-	
-	link.selectAll("linearGradient")
-        .attr("x1", d => d.source.x)
-        .attr("y1", d => d.source.y)
-        .attr("x2", d => d.target.x)
-        .attr("y2", d => d.target.y);
-	
-	link.selectAll("line")
-        .attr("x1", d => d.source.x)
-        .attr("y1", d => d.source.y)
-        .attr("x2", d => d.target.x)
-        .attr("y2", d => d.target.y);
-	
-	link.selectAll("g")
-        .attr("x1", d => d.source.x)
-        .attr("y1", d => d.source.y)
-        .attr("x2", d => d.target.x)
-        .attr("y2", d => d.target.y);
-	
-	link.selectAll("linearGradient")
-        .attr("x1", d => d.source.x)
-        .attr("y1", d => d.source.y)
-        .attr("x2", d => d.target.x)
-        .attr("y2", d => d.target.y);
-	
-	link.selectAll("line")
-        .attr("x1", d => d.source.x)
-        .attr("y1", d => d.source.y)
-        .attr("x2", d => d.target.x)
-        .attr("y2", d => d.target.y);*/
 }
 
 
