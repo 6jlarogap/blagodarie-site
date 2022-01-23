4c4ad67d772579186f47a2257a499f0bfa8476ee
NikitaLastivka <MrSuperskuy@gmail.com>
2022-01-23 04:17:05 +0200

    add

--- blagodarie/gen/assets/code.js
+++ blagodarie/gen/assets/code.js
@@ -2764,17 +2764,17 @@ async function OnfriendClickFunc(uid, nodeType){
 	async function link_to_user(){
 	 	url.searchParams.set('id', uid);
 		history.pushState(null, null, url.href);
-		await startLoad();
+		startLoad();
 		data = [];
 		nodes = [];
 		links = [];
 		links_parent = [];
-		await getApiUrl();
 		svg.remove();
 		svg = d3.select("body").append("svg")
     	        .attr("id", "main")
     	        .attr("viewBox", "0 0 " + w + " " + h )
     	        .attr("preserveAspectRatio", "xMidYMid meet");
+		getApiUrl();
 		initDefs();
 		initializeDisplay();
 		initializeSimulation();
