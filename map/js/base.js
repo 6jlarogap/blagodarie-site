051e729ccf5b2416b7da64151c9c9fc6ee90b04a
Eugene Suprun <suprune20@gmail.com>
2022-09-01 16:21:12 +0300

    map.blagodarie.org with uuid parm

--- map/js/base.js
+++ map/js/base.js
@@ -15,10 +15,33 @@ function get_api_url_() {
 
 function main_() {
     var api_url = get_api_url_();
+    var api_get_parms = [];
+    // Показ только одного юзера по центру
+    var got_parm = document.URL.match(/id=([0-9a-f\-]+)/i);
+    if (got_parm) {
+        api_get_parms.push('uuid=' + got_parm[1]);
+    }
+    // Показ всех из группы
+    got_parm = document.URL.match(/chat_id=([0-9\-]+)/i);
+    if (got_parm) {
+        api_get_parms.push('uuid=' + got_parm[1]);
+    }
+    var api_get_parm = api_get_parms.join('&');
     $.ajax({
-        url: api_url  + '/api/user/points',
+        url: api_url  + '/api/user/points/' + (api_get_parm ? '?' + api_get_parm : ''),
         dataType: 'json',
         success: function(data) {
+            console.log(data);
+            if (data.first_name) {
+                $('#id_subtitle_').html('<h2>' + data.first_name + '</h2>');
+                if (data.found_coordinates) {
+                    $('#id_error_').html('<h3>на карте</h3>');
+                } else {
+                    $('#id_error_').html('<h4>(Не задал(а) местоположение. Показаны все участники)</h4>');
+                }
+            } else {
+                $('#id_subtitle_').html('<h2><big>Наши участники</big></h2>');
+            }
             show_map(data);
         }
     });
@@ -38,10 +61,9 @@ function show_map(data) {
             maxZoom: 20,
             // Это обязательно!
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        }),
-        latlng = L.latLng(data.lat_avg, data.lng_avg);
-
-    var map = L.map('map', { center: latlng, zoom: 5, layers: [tiles] });
+        });
+    console.log(data.lat_avg, data.lng_avg);
+    var latlng = L.latLng(data.lat_avg, data.lng_avg);
     var progress = document.getElementById('progress');
     var progressBar = document.getElementById('progress-bar');
 
@@ -54,9 +76,14 @@ function show_map(data) {
         marker.bindPopup(point.popup);
         markerList.push(marker);
     }
+    var map = L.map('map', { center: latlng, zoom: (data.found_coordinates ? 10 : 5), layers: [tiles] });
     markers.addLayers(markerList);
     map.addLayer(markers);
-    map.fitBounds(markers.getBounds());
+    if (!data.found_coordinates) {
+        // Параметры center, zoom в L.map тогда не учитывается
+        // Показываем всех
+        map.fitBounds(markers.getBounds());
+    }
 }
 
 function updateProgressBar(processed, total, elapsed, layersArray) {
