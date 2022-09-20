f6ff54b2c27a2093a35e285e4ffecdb6ea24988a
Eugene Suprun <suprune20@gmail.com>
2022-09-20 19:22:48 +0300

    map, chat_id parm

--- map/js/base.js
+++ map/js/base.js
@@ -16,15 +16,16 @@ function get_api_url_() {
 function main_() {
     var api_url = get_api_url_();
     var api_get_parms = [];
-    // Показ только одного юзера по центру
-    var got_parm = document.URL.match(/id=([0-9a-f\-]+)/i);
-    if (got_parm) {
-        api_get_parms.push('uuid=' + got_parm[1]);
-    }
     // Показ всех из группы
-    got_parm = document.URL.match(/chat_id=([0-9\-]+)/i);
+    var got_parm = document.URL.match(/chat_id=([0-9\-]+)/i);
     if (got_parm) {
-        api_get_parms.push('uuid=' + got_parm[1]);
+        api_get_parms.push('chat_id=' + got_parm[1]);
+    } else {
+        // Показ только одного юзера по центру
+        got_parm = document.URL.match(/uuid=([0-9a-f\-]+)/i);
+        if (got_parm) {
+            api_get_parms.push('uuid=' + got_parm[1]);
+        }
     }
     var api_get_parm = api_get_parms.join('&');
     $.ajax({
@@ -43,7 +44,23 @@ function main_() {
                     $('#id_address_').html('<h4>(Не задал(а) местоположение. Показаны все участники)</h4>');
                 }
             } else {
-                $('#id_subtitle_').html('<h2><big>Наши участники</big></h2>');
+                var subtitle = '';
+                var num_men = '(указавших свое место: ' + data.points.length +  ')';
+                if (data.chat_id) {
+                    if (data.chat_title) {
+                        subtitle =
+                            '<h3>Участники телеграм ' +
+                            (data.chat_type == 'channel' ? 'канала' : 'группы') +
+                             ' ' + num_men +
+                            '</h3>' +
+                            '<h2><a href="' + document.URL + '">' + data.chat_title + '</a></h2>';
+                    } else {
+                        subtitle = '<h2><big>Канал или группа не найден(а)</big></h2>';
+                    }
+                } else {
+                    subtitle = '<h3><big>Наши участники ' + num_men + '</big></h3>';
+                }
+                $('#id_subtitle_').html(subtitle);
             }
             show_map(data);
         }
@@ -90,10 +107,20 @@ function show_map(data) {
         marker.bindPopup(point.popup);
         markerList.push(marker);
     }
-    var map = L.map('map', { center: latlng, zoom: (data.found_coordinates ? 10 : 5), layers: [tiles] });
+    var zoom = 5;
+    if (data.found_coordinates) {
+        zoom = 10;
+    } else if (data.chat_id) {
+        if (data.points.length == 0) {
+            zoom = 2;
+        } else if (data.points.length == 1) {
+            zoom = 5;
+        }
+    }
+    var map = L.map('map', { center: latlng, zoom: zoom, layers: [tiles] });
     markers.addLayers(markerList);
     map.addLayer(markers);
-    if (!data.found_coordinates) {
+    if (!(data.found_coordinates || data.points.length <= 1)) {
         // Параметры center, zoom в L.map тогда не учитывается
         // Показываем всех
         map.fitBounds(markers.getBounds());
