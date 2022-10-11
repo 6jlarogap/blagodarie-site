49817c31ac8f5639d943456accb4412f0460f128
Eugene Suprun <suprune20@gmail.com>
2022-10-11 17:09:04 +0300

    genesis to site tree (2)

--- genesis/genesis/settings.js
+++ genesis/genesis/settings.js
@@ -16,25 +16,9 @@ const settingSets = [
         }
     },
     {
-        url: 'https://dev.blagodarie.org/',
-        api: 'https://api.dev.blagodarie.org/',
+        url: 'https://genesis.blagodarie.org/',
+        api: 'https://api.blagodarie.org/',
         bot: 'BlagodarieAuthBot',
-        yandex: {
-            client_id: 'cf3f096c100041eaa2d2642bc94a9f5b'
-        },
-        vk: {
-            client_id: '7811224',
-            redirect_uri: 'https://api.dev.blagodarie.org/api/oauth/callback/vk/'
-        },
-        ok: {
-            client_id: '512000750172',
-            redirect_uri: 'https://api.dev.blagodarie.org/api/oauth/callback/odnoklassniki'
-        }
-    },
-    {
-        url: 'https://dev2.blagodarie.org/',
-        api: 'https://api.dev.blagodarie.org/',
-        bot: 'DevBlagoBot',
         yandex: {
             client_id: 'cf3f096c100041eaa2d2642bc94a9f5b'
         },
