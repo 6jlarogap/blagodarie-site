e60e5828ea476956cfeb6f76fb9ae6320e0b7603
Eugene Suprun <suprune20@gmail.com>
2023-01-05 18:32:44 +0300

    genesis/relations/

---
+++ genesis/relations/js/base.js
@@ -0,0 +1,41 @@
+function get_api_url_() {
+    if (typeof __DEBUG__ === 'undefined' || !__DEBUG__) {
+        // variable is undefined
+        return 'https://api.blagoroda.org';
+    } else {
+        return 'http://127.0.0.1:8000';
+    }
+}
+
+function main_() {
+    const api_url = get_api_url_();
+    $.ajax({
+        url: api_url  + '/api/profile_genesis/all?fmt=3d-force-graph',
+        dataType: 'json',
+        success: function(data) {
+
+            const Graph = ForceGraph3D()
+            (document.getElementById('3d-graph'))
+            .nodeThreeObject(({ id, photo }) => {
+                const photoTexture = new THREE.TextureLoader().load(photo ? photo: `./images/quest.png`);
+                const material = new THREE.SpriteMaterial({ map: photoTexture });
+                const sprite = new THREE.Sprite(material);
+                if (photo) {
+                    sprite.scale.set(25, 25);
+                } else {
+                    sprite.scale.set(25, 25);
+                }
+                return sprite;
+            })
+            .graphData(data)
+            .linkDirectionalArrowLength(5)
+            .linkDirectionalArrowRelPos(1)
+            .linkCurvature(0.25)
+            .nodeLabel(node => `${node.first_name}`);
+        }
+    });
+}
+
+$(function() {
+    main_();
+});
