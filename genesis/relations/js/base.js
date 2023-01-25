20dc8feb4ca0b1b0ea9fd9becf432ea3c2160400
Eugene Suprun <suprune20@gmail.com>
2023-01-25 15:51:02 +0300

    /relations/? dover={on,}

--- genesis/relations/js/base.js
+++ genesis/relations/js/base.js
@@ -1,22 +1,32 @@
 function get_api_url_() {
-    if (typeof __DEBUG__ === 'undefined' || !__DEBUG__) {
-        // variable is undefined
+    if (typeof __API_URL__ === 'undefined') {
         return 'https://api.blagoroda.org';
     } else {
-        return 'http://127.0.0.1:8000';
+        return __API_URL__;
     }
 }
 
 var parm_withalone = '';
+var parm_dover = '';
 
 function main_() {
     const api_url = get_api_url_();
-    var got_parm = document.URL.match(/[\?\&]withalone\=(\w+)/i);
+    var got_parm;
+    got_parm = document.URL.match(/[\?\&]withalone\=(\w+)/i);
     if (got_parm) {
         parm_withalone = got_parm[1];
     }
+    got_parm = document.URL.match(/[\?\&]dover\=(\w+)/i);
+    if (got_parm) {
+        parm_dover = got_parm[1];
+    }
     $.ajax({
-        url: api_url  + '/api/profile_genesis/all?fmt=3d-force-graph' + '&withalone=' + parm_withalone,
+        url:
+            api_url  +
+            '/api/profile_genesis/all?fmt=3d-force-graph' +
+            '&withalone=' + parm_withalone +
+            '&dover=' + parm_dover
+        ,
         dataType: 'json',
         success: function(data) {
 
@@ -36,13 +46,13 @@ function main_() {
                 return sprite;
             })
             .graphData(data)
-            .linkColor(() => '#ffe8e8')
+            .linkColor(link => link.is_child ? '#ffe8e8' : '#366b0d' )
             .linkOpacity(0.8)
             .linkCurvature(0.25)
             .nodeLabel(node => `${node.first_name}`)
             .linkDirectionalArrowLength(10)
             .linkDirectionalArrowRelPos(1)
-            .linkDirectionalArrowColor(() => 'rgba(255, 232, 232, 0.8)');
+            .linkDirectionalArrowColor(link => link.is_child ? 'rgba(255, 232, 232, 0.8)' : 'rgba(54, 107, 13, 0.8)' );
         }
     });
 }
