d12548fabb806d9bcdc06068353e2557fefa2387
Eugene Suprun <suprune20@gmail.com>
2023-03-13 21:25:50 +0300

    relations/?tg_poll_id=

--- genesis/relations/js/base.js
+++ genesis/relations/js/base.js
@@ -23,6 +23,7 @@ function get_parm(parm) {
 }
 
 var parm_rod = '';
+var parm_tg_poll_id = '';
 
 function main_() {
 
@@ -48,33 +49,46 @@ function main_() {
     if (is_group_site) {
         parm_tg_group_chat_id = get_parm('tg_group_chat_id') || '?tg_group_chat_id=0';
     } else {
-        parm_rod = get_parm('rod') || '';
-        parm_dover = get_parm('dover') || '';
-        parm_withalone = get_parm('withalone') || '';
+        parm_tg_poll_id = get_parm('tg_poll_id') || '';
+        if (!parm_tg_poll_id) {
+            parm_rod = get_parm('rod') || '';
+            parm_dover = get_parm('dover') || '';
+            parm_withalone = get_parm('withalone') || '';
+        }
     }
     const api_url = get_api_url_();
-    const api_get_parms =  is_group_site
-        ?
+    var api_get_parms;
+    if (parm_tg_poll_id) {
+        api_get_parms =
+            '/api/bot/poll/results/?tg_poll_id=' + parm_tg_poll_id;
+    } else if (is_group_site) {
+        api_get_parms =
             '/api/getstats/user_connections_graph?fmt=3d-force-graph' +
             '&number=0' +
-            '&tg_group_chat_id=' + parm_tg_group_chat_id
-        :
+            '&tg_group_chat_id=' + parm_tg_group_chat_id;
+    } else {
+        api_get_parms =
             '/api/profile_genesis/all?fmt=3d-force-graph' +
             '&withalone=' + parm_withalone +
             '&dover=' + parm_dover +
-            '&rod=' + parm_rod
-        ;
+            '&rod=' + parm_rod;
+    }
     $.ajax({
         url: api_url  + api_get_parms,
         dataType: 'json',
         success: function(data) {
-
+            if (parm_tg_poll_id && data.question) {
+                document.title = 'Опрос: ' + data.question;
+            }
             const photoTextureUnknown = new THREE.TextureLoader().load(`./images/star.jpeg`);
             const Graph = ForceGraph3D()
             (document.getElementById('3d-graph'))
             .nodeThreeObject(({ id, photo }) => {
                 var photoTexture;
-                if (photo) {
+                if (photo == 'poll-answer') {
+                    photoTexture = new THREE.TextureLoader().load('./images/poll-answers/poll-answer_' + (Math.abs(id).toString()) + '.png');
+                }
+                else if (photo) {
                     photoTexture = new THREE.TextureLoader().load(photo);
                 } else {
                     photoTexture = photoTextureUnknown;
@@ -87,17 +101,18 @@ function main_() {
             .graphData(data)
             // Если есть и родственная связь, и доверие, и если задано
             // искать родственные связи, то показываем стрелку цвета родственной связи
-            .linkColor(link => link.is_child && parm_rod ? '#ffe8e8' : '#366b0d' )
+            .linkColor(link => (link.is_poll || link.is_child && parm_rod) ? '#ffe8e8' : '#366b0d' )
             .linkOpacity(0.8)
             .linkCurvature(0.25)
             .nodeLabel(node => `${node.first_name}`)
             .linkDirectionalArrowLength(10)
             .linkDirectionalArrowRelPos(1)
             .linkDirectionalArrowColor(
-                link => link.is_child && parm_rod ? 'rgba(255, 232, 232, 0.8)' : 'rgba(54, 107, 13, 0.8)'
-            )
-            .d3Force('charge').strength(-320)
-            ;
+                link => (link.is_poll || link.is_child && parm_rod) ? 'rgba(255, 232, 232, 0.8)' : 'rgba(54, 107, 13, 0.8)'
+            );
+            if (!parm_tg_poll_id) {
+                Graph.d3Force('charge').strength(-320);
+            }
         }
     });
 }
