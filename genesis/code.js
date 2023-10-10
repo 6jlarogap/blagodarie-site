ab8d562901a2690cc9a5b86f98155fd30277b77c
Благодар <62824780+6jlarogap@users.noreply.github.com>
2023-10-10 09:16:47 +0300

    Update code.js

--- genesis/code.js
+++ genesis/code.js
@@ -505,7 +505,7 @@ d3.json(apiUrl, d3_json_parms)
 		.force("x", d3.forceX(width / 2).strength(0.1))
 		.force("y", d3.forceY(height / 2).strength(0.1))
 		.force("link", d3.forceLink(links).id(d => d.id).iterations(100).distance(300).strength(1))
-		.force("charge", d3.forceManyBody().strength(-10000).distancemax(100))
+		.force("charge", d3.forceManyBody().strength(-10000).distanceMax(100))
 		.force("collide", d3.forceCollide().strength(1).radius(64))//.iterations(1))
 //		.force("center", d3.forceCenter(width / 2, height / 2))
 		.on("tick", ticked);
