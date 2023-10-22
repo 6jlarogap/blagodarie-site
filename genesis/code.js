166cce29f1c6aee18987c5cae65917c3c5659aca
Благодар <62824780+6jlarogap@users.noreply.github.com>
2023-10-22 15:09:46 +0300

    Update code.js

--- genesis/code.js
+++ genesis/code.js
@@ -532,19 +532,24 @@ d3.json(apiUrl, d3_json_parms)
 		simulation = d3.forceSimulation(nodes);
 		simulation.force("link", d3.forceLink(links).id(d => d.id).strength(0.6));
 		simulation.force("charge", d3.forceManyBody().strength(-450));
-	//	simulation.force("collide", d3.forceCollide().strength(5).radius(20));//.iterations(1));//radius 80  strength(0.6)
 		simulation.force("x", d3.forceX(width / 2));
 		simulation.force("y", d3.forceY(height / 2));		
+	//	simulation.force("collide", d3.forceCollide().strength(5).radius(20));//.iterations(1));//radius 80  strength(0.6)
 	}
 	else{
 		simulation = d3.forceSimulation(nodes);
+		simulation.force("link", d3.forceLink(links).id(d => d.id).strength(0.6));
+		simulation.force("charge", d3.forceManyBody().strength(-450));
+		simulation.force("x", d3.forceX(width / 2));
+		simulation.force("y", d3.forceY(height / 2));		
+		simulation.force("collide", d3.forceCollide().strength(1).radius(95));
+/*
 		simulation.force("link", d3.forceLink(links).id(d => d.id).iterations(200).strength(1).distance(300));
 //		simulation.force("charge", d3.forceManyBody().strength(-980));
 		simulation.force("x", d3.forceX(width / 2));//.strength(0.025));
 		simulation.force("y", d3.forceY(height / 2));//.strength(0.025));		
-		simulation.force("collide", d3.forceCollide().strength(1).radius(95))
-/*
-		simulation.force("x", d3.forceX(width / 2).strength(0.03))
+
+  		simulation.force("x", d3.forceX(width / 2).strength(0.03))
 		simulation.force("y", d3.forceY(height / 2).strength(0.03))
 		simulation.force("link", d3.forceLink(links).id(d => d.id).iterations(100).distance(500).strength(0.1))
 		simulation.force("charge", d3.forceManyBody().strength(-800)) //.distanceMax(500))
