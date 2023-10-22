afe9471bd73351f06590643f5c9156dd048019ea
Благодар <62824780+6jlarogap@users.noreply.github.com>
2023-10-22 11:28:17 +0300

    Update code.js

--- genesis/code.js
+++ genesis/code.js
@@ -540,8 +540,8 @@ d3.json(apiUrl, d3_json_parms)
 		simulation = d3.forceSimulation(nodes);
 		simulation.force("link", d3.forceLink(links).id(d => d.id).strength(0.6));
 		simulation.force("charge", d3.forceManyBody().strength(-50));
-		simulation.force("x", d3.forceX(width / 2).strength(0.03));
-		simulation.force("y", d3.forceY(height / 2).strength(0.03));		
+		simulation.force("x", d3.forceX(width / 2).strength(0.02));
+		simulation.force("y", d3.forceY(height / 2).strength(0.02));		
 		simulation.force("collide", d3.forceCollide().strength(1).radius(80))//.iterations(1))
 /*
 		simulation.force("x", d3.forceX(width / 2).strength(0.03))
