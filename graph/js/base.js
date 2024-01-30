d4cbc715ff48c1242d04d3949194d1c4770fcc67
Eugene Suprun <suprune20@gmail.com>
2024-01-30 13:57:45 +0300

    Граф доверий юзера. Фокусировка в комментариях

--- graph/js/base.js
+++ graph/js/base.js
@@ -1049,9 +1049,26 @@ $(document).ready (async function() {
             Graph(graph_container).graphData(data);
             if (
                 parm_user_uuid_trusts && auth_data && data.root_node &&
-                data.root_node.id != auth_data.user_id
+                data.root_node.uuid != auth_data.user_uuid
                ) {
-                show_popup(data.root_node);
+                const node = data.root_node
+                /*
+                // Попытка фокусировки
+                //
+                const distance = 40;
+                const distRatio = 1 + distance/Math.hypot(node.x, node.y, node.z);
+
+                const newPos = node.x || node.y || node.z
+                    ? { x: node.x * distRatio, y: node.y * distRatio, z: node.z * distRatio }
+                    : { x: 0, y: 0, z: distance }; // special case if node is in (0,0,0)
+
+                Graph.cameraPosition(
+                    newPos, // new position
+                    node, // lookAt ({ x, y, z })
+                    3000  // ms transition duration
+                );
+                */
+                show_popup(node);
             }
         }
     }
