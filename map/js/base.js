025f732d976a4a345cf62fd0fdfc4ef646068821
Eugene Suprun <suprune20@gmail.com>
2024-09-17 13:37:14 +0300

    map, reload markers at drag/zoom, 2

--- map/js/base.js
+++ map/js/base.js
@@ -103,7 +103,7 @@ $(document).ready (async () => {
     }
 
     const markers = L.markerClusterGroup({ chunkedLoading: true, chunkProgress: updateProgressBar });
-    const fill_markerList = (points) => {
+    function fill_markerList(points) {
         const markerList = [];
         for (let i = 0; i < points.length; i++) {
             let point = points[i];
