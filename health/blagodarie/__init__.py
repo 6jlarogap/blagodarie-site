51f30ca27127e6218c742e82583cef7a3f1ea037
Eugene Suprun <suprune20@gmail.com>
2024-11-11 13:41:18 +0300

    health: dash deprecation fixes

--- health/blagodarie/__init__.py
+++ health/blagodarie/__init__.py
@@ -1,8 +1,8 @@
 #!../ENV/bin/python3
 
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc
+from dash import html
 from dash.dependencies import Input, Output
 import plotly.graph_objects as go
 
