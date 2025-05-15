2b301dda282a55f9f179d95fd7f46108032f6e5f
Eugene Suprun <suprune20@gmail.com>
2025-05-15 11:50:50 +0300

    health, fix obsolete

--- health/blagodarie/__init__.py
+++ health/blagodarie/__init__.py
@@ -455,5 +455,5 @@ def update_chart_users_symptoms(search, value):
 server = app.server
 
 if __name__ == '__main__':
-    app.run_server(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
 
